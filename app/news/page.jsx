abb7e6386dbabfa3ca25af5dbf400850fdff8a0c
4nglp <4nglpw@gmail.com>
2024-12-24 18:07:51 +0100

    added the news page

--- app/news/page.jsx
+++ app/news/page.jsx
@@ -1,7 +1,28 @@
 import React from "react";
 
 const News = () => {
-  return <div>News</div>;
+  return (
+    <section className="py-5 pt-3">
+      <article>
+        <h2 className="text-yellow-500">
+          Hamizza's Bizarre Adventure: the movie
+        </h2>
+        <p className="text-center">CUMMING SOON...!</p>
+        <p className="text-center">trust</p>
+        <div className="mt-2 text-center">
+          <iframe
+            className="max-w-full max-h-full mx-auto"
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/LV9UYtFoLQU"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
+        </div>
+      </article>
+    </section>
+  );
 };
 
 export default News;
